681fa660e8fe4dedaefa0ed6b195162d8d139853
Brandon Lee <99155935+DredgenYors@users.noreply.github.com>
2022-05-23 09:42:44 -0400

    Update echo.cpp

--- EchoEchoEchoKattis/EchoEchoEchoKattis/echo.cpp
+++ EchoEchoEchoKattis/EchoEchoEchoKattis/echo.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-	string echoword;
+	string echoword; //declares variable of type string
 
-		cout << "Yell into the cave: " << endl;
-		cin >> echoword; 
+		cout << "Yell into the cave: " << endl; //prompts user to enter input
+		cin >> echoword; //accepts user string input
 
-		while (echoword.empty())
+		while (echoword.empty()) //while statement to ensure that there is an input, or that the string value will be greater than "0"
 			cout << "You didn't yell!";
 
-		string limitecho = echoword.substr(0, 14);
+		string limitecho = echoword.substr(0, 14); //new string that limits the size of user string input echoword to no more than 15 characters
 
-		cout << limitecho << " " << limitecho << " " << limitecho;
+		cout << limitecho << " " << limitecho << " " << limitecho; //output of the new string 3 times
 
 }
